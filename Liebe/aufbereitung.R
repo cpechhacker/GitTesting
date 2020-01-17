41fcb3018578b0b60cd1c60c8f326ca6c97ab59e
Christian Pechhacker <christian.pechhacker@realevalue.at>
2020-01-17 08:52:02 +0100

    how to save files

--- Liebe/aufbereitung.R
+++ Liebe/aufbereitung.R
@@ -5,3 +5,14 @@ library(tidyverse)
 source("FunctionsLibrary/math_functions.R")
 
 add_zahlen(5, 4)
+
+
+
+random_numbers <- tibble( x= rnorm(1000), y = runif(1000))
+
+
+## Save 
+
+save(file = "S:/Sonstige_Projekte/TestProjects/Data/V1/randoms_numbers.Rdata", list = c("random_numbers")) 
+## oder
+save(file = "../../Data/V1/randoms_numbers.Rdata", list = c("random_numbers")) 
